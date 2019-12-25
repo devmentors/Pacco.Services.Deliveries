9883ae2d88ab9c472afed5c55277f1cb738ec495
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-12-25 12:23:35 +0100

    Updated Outbox

--- src/Pacco.Services.Deliveries.Infrastructure/Extensions.cs
+++ src/Pacco.Services.Deliveries.Infrastructure/Extensions.cs
@@ -13,6 +13,7 @@ using Convey.LoadBalancing.Fabio;
 using Convey.MessageBrokers;
 using Convey.MessageBrokers.CQRS;
 using Convey.MessageBrokers.Outbox;
+using Convey.MessageBrokers.Outbox.Mongo;
 using Convey.MessageBrokers.RabbitMQ;
 using Convey.Metrics.AppMetrics;
 using Convey.Persistence.MongoDB;
@@ -60,7 +61,7 @@ namespace Pacco.Services.Deliveries.Infrastructure
                 .AddConsul()
                 .AddFabio()
                 .AddRabbitMq(plugins: p => p.AddJaegerRabbitMqPlugin())
-                .AddMessageOutbox()
+                .AddMessageOutbox(o => o.AddMongo())
                 .AddExceptionToMessageMapper<ExceptionToMessageMapper>()
                 .AddMongo()
                 .AddRedis()
